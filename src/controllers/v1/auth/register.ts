3c40212b78c4fbbef52dba8e3288dfa5d78e9f5b
Heera Singh <heera.madquick@gmail.com>
2025-12-24 00:22:32 +0530

    user registration

--- src/controllers/v1/auth/register.ts
+++ src/controllers/v1/auth/register.ts
@@ -1,16 +1,39 @@
-import { logger } from "@/lib/winston";
-import config from "@/config";
-
 import type { Request, Response } from "express";
+import { logger } from "@/lib/winston";
+import user, { IUser } from "@/models/user";
+
+import config from "@/config";
+import { getUsername } from "@/utils";
+
+type UserData = Pick<IUser, "email" | "password" | "role">;
 
 const register = async (req: Request, res: Response): Promise<void> => {
+  const { email, password, role } = req.body as UserData;
+
   try {
-    res.status(201).json({ message: "New created" });
+    const username = getUsername();
+    const newUser = await user.create({
+      username,
+      email,
+      password,
+      role,
+    });
+
+    console.log(email, password, role);
+
+    res.status(201).json({
+      message: "New created",
+      user: {
+        username: newUser.username,
+        email: newUser.email,
+        role: newUser.role,
+      },
+    });
   } catch (error) {
     res.status(500).json({
       code: "ServerError",
       message: "Internal server error",
-      error,
+      error: error,
     });
 
     logger.error("Error during user registration", error);
